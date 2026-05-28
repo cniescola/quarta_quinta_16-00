c8bc2b0c1f81886f3b06671c6bd87a2b77d51f9c
mabel <isabelcesario17@gmail.com>
2026-05-28 17:24:11 -0300

    fizemos um exercicio de criação de um formulario com cadastro de email e senha

---
+++ exercicio/recebe.php
@@ -0,0 +1,10 @@
+<?php
+    if(isset($_POST['email'])){
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        $host = "localhost:3306";
+        $usuario = "root";
+        
+    }
+?>
