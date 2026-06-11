a915e2082c3342e1166c05b8602fdc9e628a4652
Arthur <arturferreiramoraes@327gmail.com>
2026-06-11 17:24:33 -0300

    refazendo o codigo que nao commitei certo mas realizamos tudo

---
+++ exercicio/exercicionovo.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>cadastro login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <main class="container-fluid d-flex justify-content-center align-items-center w-100" style="height:100vh;">
+      <section class="container d-flex justify-content-center">
+          <form action="recebra.php" method="post" class="card p-4 col-4" style="box-shadow:3px 5px 16px #00000038">
+            <h3 class="mb-5 text-center">Cadastre-se</h3>
+            <div class="row mb-3">
+              <label class="form-label">Usuario:</label>
+              <input type="email" name="usuario" class="form-control">
+            </div>
+            <div class="row mb-3">
+              <label class="form-label">Senha:</label>
+              <input type="password" name="codigo" class="form-control">
+            </div>
+            <div class="row">
+              <input type="submit" class="btn btn-success">
+            </div>
+          </form>
+      </section>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ exercicio/recebra.php
@@ -0,0 +1,36 @@
+<?php
+    if(isset($POST['usuario'])){
+        $usurario = $_POST['USUARIO'];
+        $codigo = $_POST['CODIGO'];
+
+
+        $user = "root";
+        $senha = "cniaraguari85";
+        $host = "localhost:3306";
+        $db = "dbcodino";
+
+        $con = new mysqli($host,$user,$senha,$db);
+
+        $cadastra = "INSERT INTO `clientes` (`id`,`USUARIO`,`CODIGO`) VALUES (NULL,'$usuario','$codigo')";
+    
+        $cadastra = mysqli_query($con,$cadastra);
+
+        if(mysqli_affected_rows($con)){
+            echo "cadastro reslizado com sucesso";
+            
+        }else{
+            echo "Não realizado";
+        }
+
+    }
+?>
+
+<!-- use `dbcodino`;
+
+create table `clientes`(
+ `id` INT NOT null primary key auto_increment,
+  `USUARIO` VARCHAR (45) NOT NULL,
+  `CODIGO` INT NOT NULL
+);
+SELECT * FROM (`clientes`); -->
+
