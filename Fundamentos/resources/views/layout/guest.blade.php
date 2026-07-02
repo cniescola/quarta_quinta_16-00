f25b8f21c9fdc156365a17c95037ea6707af2c43
professor <cniescola85@gmail.com>
2026-07-02 17:24:21 -0300

    finish

---
+++ Fundamentos/resources/views/layout/guest.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
+  <body class="font-sans antialised bg-gym-dark">
+    <div class="flex min-h-screen">
+        <div class="lg:flex lg:w-1/2 bg-regal-blue">teste</div>
+        <div class="lg:flex lg:w-1/2 bg-regal-blue">teste</div>
+    </div>
+  </body>
+</html> 
